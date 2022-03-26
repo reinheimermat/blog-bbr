f3d410d33a67160405b23ed9e979b23e9f5621e1
reinheimermat <mreinheimer0@gmail.com>
2022-03-25 21:14:43 -0300

    commit

--- Js/teste.js
+++ Js/teste.js
@@ -1,8 +1,16 @@
-const ParOuImpar = (n1) => {
-    if (n1 % 2 == 0) {
-        return "par"
+function EnviarFormulario () {
+    const name = document.querySelector('#name')
+    const email = document.querySelector('#email')
+    const password = document.querySelector('#password')
+
+    if (name == "") {
+        alert ("Preencha o campo Nome!")
+    } else if (email == "") {
+        alert ("preencha o campo Email!")
+    } else if (password == "") {
+        alert ("preencha o campo Senha!")
     } else {
-        return "impar"
+        alert ("Cadastrado com sucesso!")
+        window.location.href = 'https://reinheimermat.github.io/Blog-B.B.R/html/index.html'
     }
 }
-console.log(parouimpar(10))
